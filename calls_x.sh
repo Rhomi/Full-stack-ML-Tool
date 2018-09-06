321272db29da877ceff0f67470170eb21eb1763e
Deepak Kakkattil George <rhomielijah@gmail.com>
2018-09-05 23:41:06 -0400

    Bash scripts for mac.

---
+++ calls_x.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+HEADER="Content-Type: application/json"
+URL="http://0.0.0.0:4000"
+EXP_NAME="nn_exp10"
+TRAIN_SPLIT="75"
+TYPE="ann"
+FILE_NAME="churn_modelling.csv"
+create_data=$(cat <<EOF
+{
+ "exp_name": "$EXP_NAME",
+ "train_split": "$TRAIN_SPLIT",
+ "type": "$TYPE"
+}
+EOF
+)
+model_data=$(cat <<EOF
+{
+ "file_name": "$FILE_NAME",
+ "exp_name": "$EXP_NAME"
+}
+EOF
+)
+echo "curl -X POST $HEADER -d $data $URL/experiments"
+curl -X POST -H "$HEADER" -d "$create_data" "$URL"/experiments
+curl -X POST -H "$HEADER" -d "$model_data" "$URL"/train
+curl -X POST -H "$HEADER" -d "$model_data" "$URL"/test
+
+
+
--- index.py
+++ index.py
@@ -43,5 +43,5 @@ def static_proxy(path):
 if __name__ == '__main__':
     LOG.info('running environment: %s', os.environ.get('ENV'))
     app.config['DEBUG'] = os.environ.get('ENV') == 'development' # Debug mode if development env
-    app.run(host='0.0.0.0', port=int(PORT)) # Run the app
+    app.run(host='0.0.0.0', port=int(PORT), threaded=True) # Run the app
     #app.run(host='0.0.0.0', port=4000)
